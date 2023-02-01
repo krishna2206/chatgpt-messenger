4aa02e003ffedc7ff63839fa35d3ec92d802e03b
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2023-02-01 22:52:07 +0300

    fix myself

--- chatbot/packages/myself/myself.py
+++ chatbot/packages/myself/myself.py
@@ -16,13 +16,13 @@ chatbot = Chatbot(api_key=os.getenv("OPENAI_API_TOKEN"))
 
 @block_successive_actions
 def fallback(recipient_id):
-    user = User()
+    user = User(recipient_id)
     send_api.send_text_message("I'm sorry, I don't understand", recipient_id)
 
 
 @block_successive_actions
 def respond_to_user(prompt, recipient_id):
-    user = User()
+    user = User(recipient_id)
 
     chatbot.load_conversation_history()
     response = chatbot.ask(prompt)
