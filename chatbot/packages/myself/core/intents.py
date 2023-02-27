9861595d8ba7b658f6c0fd7ee310b0d3d4e815dd
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2023-02-27 20:10:06 +0300

    Commit: Add welcome_message, ask_clear_conversation_context and clear_conversation_context intents, and update respond_to_user intent

--- chatbot/packages/myself/core/intents.py
+++ chatbot/packages/myself/core/intents.py
@@ -1,4 +1,4 @@
-from chatbot.packages.myself.myself import fallback, respond_to_user
+from chatbot.packages.myself.myself import ask_clear_conversation_context, clear_conversation_context, fallback, respond_to_user, welcome_message
 from chatbot.packages.myself.core.parsers import get_text_message
 
 
@@ -12,5 +12,17 @@ INTENTS = [
         "name": "respond_to_user",
         "action": respond_to_user,
         "parser": get_text_message
+    },
+    {
+        "name": "welcome_message",
+        "action": welcome_message,
+    },
+    {
+        "name": "ask_clear_conversation_context",
+        "action": ask_clear_conversation_context,
+    },
+    {
+        "name": "clear_conversation_context",
+        "action": clear_conversation_context,
     }
 ]
--- chatbot/packages/myself/myself.py
+++ chatbot/packages/myself/myself.py
@@ -1,9 +1,10 @@
-from revChatGPT.V1 import Chatbot
+from revChatGPT.V1 import Chatbot, Error
 
 from config import CHATGPT_MODE, ADMIN_USER_ID
 
 from chatbot.user import User
-from chatbot.sharedinstances import send_api
+from chatbot.utils import Payload
+from chatbot.sharedinstances import send_api, msgr_api_components
 
 from .core.logic import load_config, divide_text
 from .core.chatgptusers_model import ChatGPTUserModel
@@ -21,9 +22,54 @@ def fallback(recipient_id):
         recipient_id)
 
 
+def welcome_message(recipient_id):
+    send_api.send_text_message(
+        "Bienvenue ! Je suis ChatGPT, un modèle de langage développé par OpenAI. Je suis conçu pour comprendre et générer du langage naturel, ce qui me permet de communiquer avec les utilisateurs comme vous. Mon but est d'aider les gens en répondant à leurs questions et en fournissant des informations précises et utiles. Si vous avez des questions, n'hésitez pas à me demander!",
+        recipient_id
+    )
+
+
+def ask_clear_conversation_context(recipient_id: str):
+    quickreplies = msgr_api_components.QuickReplies()
+
+    yes_quickrep = msgr_api_components.QuickReply(
+        title="✅ Oui",
+        payload=Payload(
+            target_action="clear_conversation_context",
+            clear="true").get_content()
+    )
+    quickreplies.add_quick_reply(yes_quickrep.get_content())
+
+    no_quickrep = msgr_api_components.QuickReply(
+        title="❌ Non",
+        payload=Payload(
+            target_action="clear_conversation_context",
+            clear="false").get_content()
+    )
+    quickreplies.add_quick_reply(no_quickrep.get_content())
+
+    send_api.send_quick_replies(
+        "⚠️ Voulez-vous vraiment effacer le contexte de la conversation ? (Le chatbot va oublier ce qu'il a appris)",
+        quickreplies.get_content(),
+        recipient_id)
+
+
+def clear_conversation_context(clear: str, recipient_id: str):
+    if clear == "true":
+        chatgptuser_model.delete_user(recipient_id)
+
+        send_api.send_text_message(
+            "✅ Le contexte de la conversation a été effacé.",
+            recipient_id)
+    else:
+        send_api.send_text_message(
+            "⛔ Le contexte de la conversation n'a pas été effacé.",
+            recipient_id)
+
+
 @safe_execute_action
 @block_successive_actions
-def respond_to_user(prompt, recipient_id):
+def respond_to_user(prompt: str, recipient_id: str):
     user = User(recipient_id)
 
     try:
@@ -80,26 +126,41 @@ def __V1_respond_to_user(
     message = ""
     conversation_id = None
     parent_id = None
-    for data in chatbot.ask(prompt):
-        message = data["message"]
-        conversation_id = data["conversation_id"]
-        parent_id = data["parent_id"]
+    try:
+        for data in chatbot.ask(prompt):
+            message = data["message"]
+            conversation_id = data["conversation_id"]
+            parent_id = data["parent_id"]
+    except Error as error:
+        try:
+            error_code = int(error.code)
+        except ValueError:
+            error_code = 524
 
-    if message == "":
-        send_api.send_text_message(
-            "⚠️ Trop de requêtes en 1 heure, veuillez réessayer plus tard.",
-            recipient_id)
-
-    else:
-        if len(message) > 2000:
-            segments = divide_text(message)
-            for segment in segments:
-                send_api.send_text_message(
-                    segment,
-                    recipient_id)
+        if error_code == 524:
+            send_api.send_text_message(
+                "⚠️ Le serveur est actuellement surchargé, veuillez réessayer plus tard.",
+                recipient_id)
         else:
             send_api.send_text_message(
-                message,
+                f"⚠️ Une erreur est survenue ! Code d'erreur : {error_code}",
                 recipient_id)
+    else:
+        if message == "":
+            send_api.send_text_message(
+                "⚠️ Trop de requêtes en 1 heure, veuillez réessayer plus tard.",
+                recipient_id)
+
+        else:
+            if len(message) > 2000:
+                segments = divide_text(message)
+                for segment in segments:
+                    send_api.send_text_message(
+                        segment,
+                        recipient_id)
+            else:
+                send_api.send_text_message(
+                    message,
+                    recipient_id)
 
     return conversation_id, parent_id
